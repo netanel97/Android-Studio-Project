3b406ab2f7d5d65f3b83a1d9870cb13a7c25e17f
NetanelHabas <netanelhabas@gmail.com>
2022-11-25 08:38:30 +0200

    Vibrate update

--- app/src/main/java/com/example/hw1_netanelhabas/MainActivity.java
+++ app/src/main/java/com/example/hw1_netanelhabas/MainActivity.java
@@ -204,12 +204,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void vibrate() {
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        // Vibrate for 500 milliseconds
+        // Vibrate for 400 milliseconds
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
         } else {
             //deprecated in API 26
-            v.vibrate(200);
+            v.vibrate(400);
         }
 
 
